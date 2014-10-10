83ca923d2dd8043e1110c5d7dd1e0d74735ddcf5
Xah Lee <xah@xahlee.org>
2014-10-10 07:02:45 -0700

    Happy Hacking today, Happy Hacking everyday http://xahlee.info/kbd/idiocy_of_happy_hacking_keyboard.html

--- xah-fly-keys-emacs-1k-keymap.el
+++ xah-fly-keys-emacs-1k-keymap.el
@@ -119,9 +119,9 @@
 (global-set-key (kbd "<menu> 3") 'delete-other-windows)
 (global-set-key (kbd "<menu> 4") 'split-window-vertically)
 (global-set-key (kbd "<menu> 5") 'mark-whole-buffer)
-(global-set-key (kbd "<menu> 6") 'xah-select-current-block)
-(global-set-key (kbd "<menu> 7") 'xah-select-current-line)
-(global-set-key (kbd "<menu> 8") 'xah-select-text-in-bracket-or-quote)
+(global-set-key (kbd "<menu> 6") nil)
+(global-set-key (kbd "<menu> 7") nil)
+(global-set-key (kbd "<menu> 8") nil)
 (global-set-key (kbd "<menu> 9") 'ispell-word)
 (global-set-key (kbd "<menu> 0") nil)
 
