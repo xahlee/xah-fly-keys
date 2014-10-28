929b8b312b9fadeb2dd2faa8989156526aa005ed
Xah Lee <xah@xahlee.org>
2014-10-28 12:32:41 -0700

    added clojure temp solution to xah-run-current-file. removed Ctrl+0

--- xah-fly-keys-misc-commands.el
+++ xah-fly-keys-misc-commands.el
@@ -369,17 +369,18 @@ Version 2014-10-13"
 
 (defun xah-run-current-file ()
   "Execute the current file.
-For example, if the current buffer is the file xx.py,
-then it'll call “python xx.py” in a shell.
+For example, if the current buffer is the file xx.py, then it'll call 「python xx.py」 in a shell.
 The file can be php, perl, python, ruby, javascript, bash, ocaml, vb, elisp.
 File suffix is used to determine what program to run.
 
 If the file is modified, ask if you want to save first.
 
-If the file is emacs lisp, run the byte compiled version if exist."
+URL `http://ergoemacs.org/emacs/elisp_run_current_file.html'
+version 2014-10-28"
   (interactive)
   (let* (
-         (suffixMap
+         (ξsuffixMap
+          ;; (‹extension› . ‹shell program name›)
           `(
             ("php" . "php")
             ("pl" . "perl")
@@ -388,24 +389,25 @@ If the file is emacs lisp, run the byte compiled version if exist."
             ("rb" . "ruby")
             ("js" . "node") ; node.js
             ("sh" . "bash")
+            ("clj" . "java -cp /home/xah/apps/clojure-1.6.0/clojure-1.6.0.jar clojure.main")
             ("ml" . "ocaml")
             ("vbs" . "cscript")
             ;; ("pov" . "/usr/local/bin/povray +R2 +A0.1 +J1.2 +Am2 +Q9 +H480 +W640")
             ))
-         (fName (buffer-file-name))
-         (fSuffix (file-name-extension fName))
-         (progName (cdr (assoc fSuffix suffixMap)))
-         (cmdStr (concat progName " \""   fName "\"")))
+         (ξfName (buffer-file-name))
+         (ξfSuffix (file-name-extension ξfName))
+         (ξprogName (cdr (assoc ξfSuffix ξsuffixMap)))
+         (ξcmdStr (concat ξprogName " \""   ξfName "\"")))
 
     (when (buffer-modified-p)
       (when (y-or-n-p "Buffer modified. Do you want to save first?")
         (save-buffer)))
 
-    (if (string-equal fSuffix "el") ; special case for emacs lisp
-        (load (file-name-sans-extension fName))
-      (if progName
+    (if (string-equal ξfSuffix "el") ; special case for emacs lisp
+        (load ξfName)
+      (if ξprogName
           (progn
             (message "Running…")
-            (shell-command cmdStr "*xah-run-current-file output*" ))
+            (shell-command ξcmdStr "*xah-run-current-file output*" ))
         (message "No recognized program file suffix for this file.")))))
 
