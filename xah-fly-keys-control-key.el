5ced13686b2b0bfbf575dc6aed0e6a38ac45bb53
Xah Lee <xah@xahlee.org>
2015-04-08 11:00:34 -0700

    freedom of the Control

--- xah-fly-keys-control-key.el
+++ xah-fly-keys-control-key.el
@@ -17,21 +17,20 @@
   (global-set-key (kbd "C-8") nil)
   (global-set-key (kbd "C-9") nil)
   (global-set-key (kbd "C-0") nil)
-
   )
 
-;; (progn                                            ; standard keys
-;;   (global-set-key (kbd "C-a") 'mark-whole-buffer) ; was move-beginning-of-line
-;;   (global-set-key (kbd "C-n") 'xah-new-empty-buffer) ; was next-line
-;;   (global-set-key (kbd "C-S-n") 'make-frame-command) ; was nil
-;;   (global-set-key (kbd "C-o") 'find-file)            ; was open-line
-;;   (global-set-key (kbd "C-s") 'save-buffer)  ; was isearch-forward
-;;   (global-set-key (kbd "C-S-s") 'write-file) ; was nil
-;;   (global-set-key (kbd "C-S-t") 'xah-open-last-closed) ; was nil
+(progn                                            ; standard keys
+  (global-set-key (kbd "C-a") 'mark-whole-buffer) ; was move-beginning-of-line
+  (global-set-key (kbd "C-n") 'xah-new-empty-buffer) ; was next-line
+  (global-set-key (kbd "C-S-n") 'make-frame-command) ; was nil
+  (global-set-key (kbd "C-o") 'find-file)            ; was open-line
+  (global-set-key (kbd "C-s") 'save-buffer)  ; was isearch-forward
+  (global-set-key (kbd "C-S-s") 'write-file) ; was nil
+  (global-set-key (kbd "C-S-t") 'xah-open-last-closed) ; was nil
   (global-set-key (kbd "C-w") 'xah-close-current-buffer) ; was kill-region
-;;   (global-set-key (kbd "C-y") 'redo)                     ; was yank
-;;   (global-set-key (kbd "C-z") 'undo) ; was suspend-frame
-;;   )
+  (global-set-key (kbd "C-y") 'redo)                     ; was yank
+  (global-set-key (kbd "C-z") 'undo) ; was suspend-frame
+  )
 
 (progn
   (global-set-key (kbd "<C-next>") 'xah-next-user-buffer)
