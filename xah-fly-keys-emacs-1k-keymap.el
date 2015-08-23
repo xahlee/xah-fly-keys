59600b40954f3f073344d23b46e433455e2415e6
Xah Lee <xah@xahlee.org>
2015-08-23 14:52:42 -0700

    fixed previous commit

--- xah-fly-keys-emacs-1k-keymap.el
+++ xah-fly-keys-emacs-1k-keymap.el
@@ -269,6 +269,39 @@
   (define-key xah-danger-keymap (kbd "w") 'delete-frame))
 
 (progn
+  (define-prefix-command 'xah-insertion-keymap)
+
+  (define-key xah-insertion-keymap (kbd "RET") 'insert-char)
+
+  (define-key xah-insertion-keymap (kbd ",") nil)
+
+  (define-key xah-insertion-keymap (kbd "a") nil)
+  (define-key xah-insertion-keymap (kbd "b") 'xah-insert-black-lenticular-bracket【】)
+  (define-key xah-insertion-keymap (kbd "c") 'xah-insert-ascii-single-quote)
+  (define-key xah-insertion-keymap (kbd "d") 'xah-insert-double-curly-quote“”)
+  (define-key xah-insertion-keymap (kbd "e") 'xah-insert-unicode)
+  (define-key xah-insertion-keymap (kbd "f") 'xah-insert-emacs-quote)
+  (define-key xah-insertion-keymap (kbd "g") 'xah-insert-ascii-double-quote)
+  (define-key xah-insertion-keymap (kbd "h") 'xah-insert-brace) ; {}
+  (define-key xah-insertion-keymap (kbd "i") 'xah-insert-curly-single-quote‘’)
+  (define-key xah-insertion-keymap (kbd "j") nil)
+  (define-key xah-insertion-keymap (kbd "k") nil)
+  (define-key xah-insertion-keymap (kbd "m") 'xah-insert-corner-bracket「」)
+  (define-key xah-insertion-keymap (kbd "n") 'xah-insert-square-bracket) ; []
+  (define-key xah-insertion-keymap (kbd "o") nil)
+  (define-key xah-insertion-keymap (kbd "p") 'xah-insert-single-angle-quote‹›)
+  (define-key xah-insertion-keymap (kbd "q") nil)
+  (define-key xah-insertion-keymap (kbd "r") 'xah-insert-tortoise-shell-bracket〔〕)
+  (define-key xah-insertion-keymap (kbd "s") 'xah-insert-string-assignment)
+  (define-key xah-insertion-keymap (kbd "t") 'xah-insert-paren)
+  (define-key xah-insertion-keymap (kbd "u") 'xah-insert-greater-less)
+  (define-key xah-insertion-keymap (kbd "v") nil)
+  (define-key xah-insertion-keymap (kbd "w") 'xah-insert-double-angle-bracket《》)
+  (define-key xah-insertion-keymap (kbd "W") 'xah-insert-angle-bracket〈〉)
+  (define-key xah-insertion-keymap (kbd "x") nil)
+  (define-key xah-insertion-keymap (kbd "y") 'xah-insert-double-angle-quote«»))
+
+(progn
   (define-prefix-command 'xah-fly-leader-key-map)
 
   (define-key xah-fly-leader-key-map (kbd "<end>") 'xah-fly-keys)
@@ -333,40 +366,6 @@
   (define-key xah-fly-leader-key-map (kbd "x") nil)
   (define-key xah-fly-leader-key-map (kbd "y") nil)
   (define-key xah-fly-leader-key-map (kbd "z") 'comment-dwim))
-
-
-(progn
-  (define-prefix-command 'xah-insertion-keymap)
-
-  (define-key xah-insertion-keymap (kbd "RET") 'insert-char)
-
-  (define-key xah-insertion-keymap (kbd ",") nil)
-
-  (define-key xah-insertion-keymap (kbd "a") nil)
-  (define-key xah-insertion-keymap (kbd "b") 'xah-insert-black-lenticular-bracket【】)
-  (define-key xah-insertion-keymap (kbd "c") 'xah-insert-ascii-single-quote)
-  (define-key xah-insertion-keymap (kbd "d") 'xah-insert-double-curly-quote“”)
-  (define-key xah-insertion-keymap (kbd "e") 'xah-insert-unicode)
-  (define-key xah-insertion-keymap (kbd "f") 'xah-insert-emacs-quote)
-  (define-key xah-insertion-keymap (kbd "g") 'xah-insert-ascii-double-quote)
-  (define-key xah-insertion-keymap (kbd "h") 'xah-insert-brace) ; {}
-  (define-key xah-insertion-keymap (kbd "i") 'xah-insert-curly-single-quote‘’)
-  (define-key xah-insertion-keymap (kbd "j") nil)
-  (define-key xah-insertion-keymap (kbd "k") nil)
-  (define-key xah-insertion-keymap (kbd "m") 'xah-insert-corner-bracket「」)
-  (define-key xah-insertion-keymap (kbd "n") 'xah-insert-square-bracket) ; []
-  (define-key xah-insertion-keymap (kbd "o") nil)
-  (define-key xah-insertion-keymap (kbd "p") 'xah-insert-single-angle-quote‹›)
-  (define-key xah-insertion-keymap (kbd "q") nil)
-  (define-key xah-insertion-keymap (kbd "r") 'xah-insert-tortoise-shell-bracket〔〕)
-  (define-key xah-insertion-keymap (kbd "s") 'xah-insert-string-assignment)
-  (define-key xah-insertion-keymap (kbd "t") 'xah-insert-paren)
-  (define-key xah-insertion-keymap (kbd "u") 'xah-insert-greater-less)
-  (define-key xah-insertion-keymap (kbd "v") nil)
-  (define-key xah-insertion-keymap (kbd "w") 'xah-insert-double-angle-bracket《》)
-  (define-key xah-insertion-keymap (kbd "W") 'xah-insert-angle-bracket〈〉)
-  (define-key xah-insertion-keymap (kbd "x") nil)
-  (define-key xah-insertion-keymap (kbd "y") 'xah-insert-double-angle-quote«»))
 
 
 ;;;; misc
