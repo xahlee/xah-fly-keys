45d40f2f72a014d4646c0692c23c817ae1610142
Xah Lee <xah@xahlee.org>
2018-04-07 03:55:31 -0700

    corrected key for highlight-phrase

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2013-2017, by Xah Lee
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
-;; Version: 8.8.20180403
+;; Version: 8.8.20180407
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -2926,13 +2926,13 @@ Version 2017-01-21"
 
    ("." . highlight-symbol-at-point)
    ("p" . highlight-regexp)
-   ("t" . highlight-phrase)
    ("e" . unhighlight-regexp)
    ("u" . highlight-lines-matching-regexp)
 
+   ("t" . highlight-phrase)
    ("g" . isearch-forward-symbol-at-point)
+   ("c" . isearch-forward-symbol)
    ("h" . isearch-forward-word)
-   ("t" . isearch-forward-symbol)
 
    ;; temp
    ("b" . facemenu-set-bold)
