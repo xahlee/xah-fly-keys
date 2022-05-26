a3e7d6fb8b0031cf7a227af38a45879f152aee21
Xah Lee <xah@xahlee.org>
2022-05-26 00:13:58 -0700

    xah-unfill-paragraph and xah-unfill-region. revert to my magic number 90002000 of years ago. not use most-positive-fixnum.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
 ;; Maintainer: Xah Lee <xah@xahlee.org>
-;; Version: 17.9.20220516184525
+;; Version: 17.10.20220526001219
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -1131,9 +1131,9 @@ Version: 2020-11-22 2021-08-13"
 This command does the inverse of `fill-paragraph'.
 
 URL `http://xahlee.info/emacs/emacs/emacs_unfill-paragraph.html'
-Version: 2016-07-13"
+Version: 2010-05-12 2022-05-20"
   (interactive)
-  (let ((fill-column most-positive-fixnum))
+  (let ((fill-column 90002000))
     (fill-paragraph)))
 
 (defun xah-unfill-region (Begin End)
@@ -1141,9 +1141,9 @@ Version: 2016-07-13"
 This command does the inverse of `fill-region'.
 
 URL `http://xahlee.info/emacs/emacs/emacs_unfill-paragraph.html'
-Version: 2016-07-13"
+Version: 2010-05-12 2022-05-20"
   (interactive "r")
-  (let ((fill-column most-positive-fixnum))
+  (let ((fill-column 90002000))
     (fill-region Begin End)))
 
 (defun xah-change-newline-chars-to-one (Begin End)
