763f5f6458efc80fe7fc4085796bc2ecdf4b3eb5
Xah Lee <xah@xahlee.org>
2022-06-19 15:31:17 -0700

    fixed a url syntax error in doc string

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
 ;; Maintainer: Xah Lee <xah@xahlee.org>
-;; Version: 17.14.20220618104311
+;; Version: 17.14.20220618162127
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -4646,7 +4646,7 @@ Version: 2017-07-07"
   "A modal keybinding set, like vim, but based on ergonomic
   principles, like Dvorak layout.
 
-URL`http://xahlee.info/emacs/misc/xah-fly-keys.html'"
+URL `http://xahlee.info/emacs/misc/xah-fly-keys.html'"
   :group 'xah-fly-keys
   :global t
   :lighter " ∑flykeys"
