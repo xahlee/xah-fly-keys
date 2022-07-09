2383b8b546fe6e1db4b86f4122055110ee4775f4
Xah Lee <xah@xahlee.org>
2022-07-09 14:55:30 -0700

    changed key for toggle-frame-fullscreen to toggle-frame-maximized

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
 ;; Maintainer: Xah Lee <xah@xahlee.org>
-;; Version: 17.16.20220708232047
+;; Version: 17.17.20220709145456
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -4149,7 +4149,7 @@ minor modes loaded later may override bindings in this map.")
    ;;
    ("SPC" . whitespace-mode)
    ("," . abbrev-mode)
-   ("." . toggle-frame-fullscreen)
+   ("." . toggle-frame-maximized)
    ;; ("'" . )
    ;; (";" . )
 
