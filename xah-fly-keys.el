350ab789e221a709e753f7b14586fa782a8661eb
Xah Lee <xah@xahlee.org>
2022-09-09 11:03:47 -0700

    added key space h x (dvorak notation) for describe-command

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
 ;; Maintainer: Xah Lee <xah@xahlee.org>
-;; Version: 17.21.20220829140316
+;; Version: 17.22.20220909110152
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -4131,7 +4131,9 @@ minor modes loaded later may override bindings in this map.")
    ;; t
    ("u" . elisp-index-search)
    ("v" . apropos-value)
-   ;; wxy
+   ;; w
+   ("x" . describe-command) ; emacs 28
+   ;; y
    ("z" . describe-coding-system)))
 
 (xah-fly--define-keys
