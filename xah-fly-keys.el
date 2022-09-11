0422e2cfc88d92dce656e71ec0ef6adb0a773892
Xah Lee <xah@xahlee.org>
2022-09-11 11:32:21 -0700

    putting back the key C-j. that's for newline. forgot about that before.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
 ;; Maintainer: Xah Lee <xah@xahlee.org>
-;; Version: 17.23.20220910102958
+;; Version: 17.23.20220911113118
 ;; Created: 10 Sep 2013
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, emulations, vim, ergoemacs
@@ -3906,10 +3906,10 @@ minor modes loaded later may override bindings in this map.")
   (global-set-key (kbd "C-d") 'nil)
   (global-set-key (kbd "C-e") 'nil)
   (global-set-key (kbd "C-f") 'nil)
-  ;; (global-set-key (kbd "C-g") 'nil)
-  ;; (global-set-key (kbd "C-h") 'nil)
-  ;; (global-set-key (kbd "C-i") 'nil)
-  (global-set-key (kbd "C-j") 'nil)
+  ;; (global-set-key (kbd "C-g") 'nil) ; cancel
+  ;; (global-set-key (kbd "C-h") 'nil) ; help
+  ;; (global-set-key (kbd "C-i") 'nil) ; tab
+  ;; (global-set-key (kbd "C-j") 'nil) ; newline
   (global-set-key (kbd "C-k") 'nil)
   (global-set-key (kbd "C-l") 'nil)
   ;; (global-set-key (kbd "C-m") 'nil)
